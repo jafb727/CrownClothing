e8aa15f0a6862e8176cf59e4946d7ae28b748c13
Pepe <jafb727@gmail.com>
2021-01-26 00:36:12 -0800

    Crown Clothing e-commerce app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+/**
+ * Filename: index.js
+ * Author: Jose A Felix
+ * Description: index component
+ */
+
+// ----------------------------------------------------------------
+
+/** Components */
+import React from "react";
+import "./App.css";
+import Home from "./home.component";
+
+// ----------------------------------------------------------------
+
+// Main component
+function App() {
+	return (
+		<div>
+			<Home />
+		</div>
+	);
+}
+
+// Exporting component
+export default App;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ * Filename: index.js
+ * Author: Jose A Felix
+ * Description: index component
+ */
+
+// ----------------------------------------------------------------
+
+/** Components */
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+// Main component
+ReactDOM.render(<App />, document.getElementById("root"));
